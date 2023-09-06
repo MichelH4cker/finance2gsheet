e2386e652b70bc0704d264aad2360bf09aeaac53
MichelH4cker <michel.hecker@usp.br>
2023-09-06 20:46:02 -0300

    feat: break text into lines and remove unnecessary data

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,7 +1,10 @@
 from PyPDF2 import PdfReader
+import csv
 
 text = ""
 parts = []
+
+csv_data = []
 
 # jump header
 def visitor_body(text, cm, tm, font_dict, font_size):
@@ -9,15 +12,25 @@ def visitor_body(text, cm, tm, font_dict, font_size):
     if y > 50:
         parts.append(text)
 
-
+# simply read the pdf
 try:
     reader = PdfReader("extrato.pdf")
     for page in reader.pages:
-        text += page.extract_text(visitor_text=visitor_body)
+        text += page.extract_text(visitor_text = visitor_body)
         text_body = "".join(parts)
     print("[+] pdf read with success!\n")
 except Exception as exc:
     print(f"[-] Error: {exc}")
 
+# break the text into lines in a array
+lines = text_body.split('\n')
 
-print(text_body)
+# delete unnecessary data
+cleaner_lines = []
+for line in lines:
+    if "Informações Adicionais" in line:  
+        break
+    cleaner_lines.append(line)
+
+lines = cleaner_lines
+
