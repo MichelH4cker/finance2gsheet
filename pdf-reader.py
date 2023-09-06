7167021147a7cdf1763558b1781bd959744aa5e4
MichelH4cker <michel.hecker@usp.br>
2023-09-06 20:02:03 -0300

    feat: jump header read

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,13 +1,23 @@
 from PyPDF2 import PdfReader
 
 text = ""
+parts = []
+
+# jump header
+def visitor_body(text, cm, tm, font_dict, font_size):
+    y = tm[5]
+    if y > 50:
+        parts.append(text)
+
+
 try:
     reader = PdfReader("extrato.pdf")
     for page in reader.pages:
-        text += page.extract_text()
+        text += page.extract_text(visitor_text=visitor_body)
+        text_body = "".join(parts)
     print("[+] pdf read with success!\n")
 except Exception as exc:
     print(f"[-] Error: {exc}")
 
 
-print(text)
+print(text_body)
