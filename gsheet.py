ac195db2806a09d3fb25d7589f14d86efca0b405
MichelH4cker <michel.hecker@usp.br>
2023-09-07 13:21:06 -0300

    feat: send all csv file to spreadsheet on google

--- gsheet.py
+++ gsheet.py
@@ -1,8 +1,23 @@
 import gspread 
-#from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
-# myscope = ['https://spreadsheets.google.com/feeds',
-#            'https://www.googleapis.com/auth/drive']
+# setting the apis configs
+myscope = ['https://spreadsheets.google.com/feeds',
+           'https://www.googleapis.com/auth/drive']
+mycreds = ServiceAccountCredentials.from_json_keyfile_name('finance2gsheet-086d22416d93.json', myscope)
+myclient = gspread.authorize(mycreds)
 
-# mycreds = ServiceAccountCredentials.from_json_keyfile_name('finance2gsheet-086d22416d93', myscope)
-# myclient = gspread.authorize(mycreds)
+# open the spreadsheet
+worksheet = myclient.open("Finanças").sheet1
+
+# open csv file
+df = pd.read_csv("extrato.csv")
+
+print([df.columns.values.tolist()])
+
+# df.columns.values.toollist() take the name of columns
+# df.values.tolist() take the values of all rows
+
+# update the worksheet
+worksheet.update([df.columns.values.tolist()] + df.values.tolist())
