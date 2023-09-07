a5139a05b0ef25126292d30496d1904920b17498
MichelH4cker <michel.hecker@usp.br>
2023-09-06 23:19:36 -0300

    feat: format text and storage into csv file

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,10 +1,9 @@
 from PyPDF2 import PdfReader
 import csv
+import re
 
 text = ""
 parts = []
-
-csv_data = []
 
 # jump header
 def visitor_body(text, cm, tm, font_dict, font_size):
@@ -31,6 +30,66 @@ for line in lines:
     if "Informações Adicionais" in line:  
         break
     cleaner_lines.append(line)
+    #print(line)
 
 lines = cleaner_lines
 
+csv_filename = "extrato.csv"
+
+csv_data = []
+
+#  DD/MM/AAAA ou DD-MM-AAAA
+date_pattern = r'\d{2}/\d{2}/\d{4}'
+time_pattern = r'\d{2}:\d{2}'
+money_pattern = r'\d{1},\d{2}|\d{2},\d{2}|\d{3},\d{2}|\d{1}.\d{3},\d{2}'
+transaction_pattern = r'\(\+\)|\(\-\)'
+
+description_line = False
+
+with open(csv_filename, mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    # csv header
+    header = ["Data", "Valor", "Tipo de Transação", "Descrição", "Tipo de Serviço"]
+    csv_writer.writerow(header)
+
+
+    for line in range(0, len(lines)):
+        if description_line:
+            time_matches = ''.join(re.findall(time_pattern, lines[line])).strip()
+
+            # find the time position
+            time_start = lines[line].find(time_matches)
+            # take text after time
+            service_type = lines[line][time_start + len(time_matches):].strip()
+
+            row.append(service_type)
+            csv_writer.writerow(row)
+
+            description_line = False
+
+        date_matches = ''.join(re.findall(date_pattern, lines[line])).strip()
+        if (date_matches == ''):
+            continue
+
+        money_matches = ''.join(re.findall(money_pattern, lines[line])).strip()
+        Dec
+        transaction_type_matches = ''.join(re.findall(transaction_pattern, lines[line])).strip()
+
+        # find the date position
+        date_start = lines[line].find(date_matches)
+        # take text after date
+        transaction_description = lines[line][date_start + len(date_matches):].strip()
+
+        # create a matrix of datas
+        row = [date_matches, money_matches, transaction_type_matches, transaction_description]
+
+        if "Estorno de Débito" in transaction_description or "Saldo Anterior" in transaction_description:  
+            row.append("- - -")
+            csv_writer.writerow(row)
+            continue
+        else: 
+            description_line = True
+
+for row in csv_data:
+    print(row)
