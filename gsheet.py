945882769f4f6508db36186d4c08906a1422428b
MichelH4cker <michel.hecker@usp.br>
2023-09-07 12:02:05 -0300

    chore: add correct dependey in README

--- gsheet.py
+++ gsheet.py
@@ -1,7 +1,8 @@
 import gspread 
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 
-myscope = ['https://spreadsheets.google.com/feeds',
-           'https://www.googleapis.com/auth/drive']
+# myscope = ['https://spreadsheets.google.com/feeds',
+#            'https://www.googleapis.com/auth/drive']
 
-mycreds = ServiceAccountCredentials.from_json_keyfile_name()
+# mycreds = ServiceAccountCredentials.from_json_keyfile_name('finance2gsheet-086d22416d93', myscope)
+# myclient = gspread.authorize(mycreds)
